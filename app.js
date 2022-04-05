952c1ba76a0337943b8d1d298f6821079ce2a78f
andy <vy.chung@hdwebsoft.co>
2022-04-05 15:21:40 +0700

    basis setup authentication route

--- app.js
+++ app.js
@@ -4,17 +4,29 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
+// rest of the packages
+const morgan = require('morgan');
+
 // Connect Database
 const connectDB = require('./db/connect');
+
+// Routers
+const authRouter = require('./routes/authRoutes');
 
 // Error Handler Middleware Import
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+// Middleware
+app.use(morgan('tiny'))
+app.use(express.json());
+
 // Routes
 app.get('/', (req, res) => {
     res.send('E-commerce API')
 })
+
+app.use('/api/v1/auth', authRouter)
 
 // Middleware 
 app.use(notFoundMiddleware);
---
+++ routes/authRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const { register, login, logout } = require('../controllers/authController');
+
+router.post('/register', register).post('/login', login).get('/logout', logout);
+
+module.exports = router;
