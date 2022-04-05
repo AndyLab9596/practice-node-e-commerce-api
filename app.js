3bab54bc28fb06c000941f2e0ffe0c99215986d4
andy <vy.chung@hdwebsoft.co>
2022-04-05 15:02:21 +0700

    connectDB

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-console.log('E-commerce')
+require('dotenv').config();
+require('express-async-errors');
+// Express
+const express = require('express');
+const app = express();
+
+// Connect Database
+const connectDB = require('./db/connect');
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('E-commerce API')
+})
+
+// Start App Fnc
+const port = process.env.PORT || 5000;
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URL);
+        app.listen(port, () => {
+            console.log(`Server is listening on port ${port}`)
+        })
+
+    } catch (error) {
+        console.log(error)
+    }
+};
+start()
